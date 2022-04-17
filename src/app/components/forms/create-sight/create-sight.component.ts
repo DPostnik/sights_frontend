c6f993e4565d053703bf614bf964bd6c4816d3cd
Daniil Postnik <d.postnik1@gmail.com>
2022-04-17 13:11:33 +0300

    moved create-sight form to its container, added mat-button

--- src/app/components/forms/create-sight/create-sight.component.ts
+++ src/app/components/forms/create-sight/create-sight.component.ts
@@ -18,9 +18,7 @@ import {CreateSight} from '@store/actions/sights.actions';
 })
 export class CreateSightComponent implements OnInit, OnDestroy {
   @Select(AppState.selectMeta) meta$!: Observable<Meta>;
-  // @ViewChild('categoryInput') categoryInput!: ElementRef<HTMLInputElement>;
   form: FormGroup = new FormGroup({});
-  // separatorKeysCodes: number[] = [ENTER, COMMA];
 
   subscriptions: Subscription[] = [];
   countries?: Country[];
@@ -37,37 +35,17 @@ export class CreateSightComponent implements OnInit, OnDestroy {
         this.categories = meta.categories;
       }),
     );
-    // const categoriesForm = this.form.get('categories');
-    // if (categoriesForm) {
-    //   this.subscriptions.push(
-    //     categoriesForm.valueChanges
-    //       .pipe(
-    //         startWith(null),
-    //         map((value: string | null) => {
-    //           return value ? this.filteredCategories(value) : this.categories.map((item) => item.name);
-    //         }),
-    //       )
-    //       .subscribe(value => this.filteredList = value),
-    //   );
-    // }
   }
 
   constructor(private store: Store) {
     this.initializeForm();
   }
 
-  //
-  // filteredCategories(value: string) {
-  //   return this.categories
-  //     .filter((item) => this.selectedCategories.includes(item.name))
-  //     .filter((item) => item.name.includes(value))
-  //     .map(item => item.name);
-  // }
-
   private initializeForm(): void {
     this.form = new FormGroup({
       name: new FormControl(null, [Validators.required]),
       description: new FormControl(null, [Validators.required]),
+      mainImage: new FormControl(null),
       founder: new FormControl(null),
       date: new FormControl(new Date()),
       country: new FormControl(null, [Validators.required]),
@@ -92,8 +70,8 @@ export class CreateSightComponent implements OnInit, OnDestroy {
       description: this.form.get('description')?.value,
       founder: this.form.get('founder')?.value,
       city: this.form.get('city')?.value,
-      mainImage: '',
-      name: this.form.value.name,
+      name: this.form.get('name')?.value,
+      mainImage: this.form.get('mainImage')?.value,
     };
     this.store.dispatch(new CreateSight(sight));
   }
@@ -102,6 +80,9 @@ export class CreateSightComponent implements OnInit, OnDestroy {
     this.subscriptions.forEach((s) => s.unsubscribe());
   }
 
+  // @ViewChild('categoryInput') categoryInput!: ElementRef<HTMLInputElement>;
+  // separatorKeysCodes: number[] = [ENTER, COMMA];
+  //
   // onRemove(name: string) {
   //   this.selectedCategories = this.selectedCategories.filter((item) => item !== name);
   // }
@@ -120,5 +101,26 @@ export class CreateSightComponent implements OnInit, OnDestroy {
   //   this.selectedCategories.push(value);
   //   this.filteredList = this.filteredCategories(value)
   //   this.categoryInput.nativeElement.value = '';
+  // }
+  //
+  // filteredCategories(value: string) {
+  //   return this.categories
+  //     .filter((item) => this.selectedCategories.includes(item.name))
+  //     .filter((item) => item.name.includes(value))
+  //     .map(item => item.name);
+  // }
+  //
+  // const categoriesForm = this.form.get('categories');
+  // if (categoriesForm) {
+  //   this.subscriptions.push(
+  //     categoriesForm.valueChanges
+  //       .pipe(
+  //         startWith(null),
+  //         map((value: string | null) => {
+  //           return value ? this.filteredCategories(value) : this.categories.map((item) => item.name);
+  //         }),
+  //       )
+  //       .subscribe(value => this.filteredList = value),
+  //   );
   // }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -24,6 +24,7 @@ import {HeaderComponent} from '@components/header/header.component';
 import {FooterComponent} from '@components/footer/footer.component';
 import {DevMenuComponent} from '@components/devMenu/devMenu.component';
 import {MapComponent} from '@components/map/map.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -55,6 +56,7 @@ import {MapComponent} from '@components/map/map.component';
     MatChipsModule,
     MatAutocompleteModule,
     MatSelectModule,
+    MatButtonModule,
   ],
   exports: [
     HttpClientModule,
